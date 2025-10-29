0b49a8237863d5cc281f6056c718275dc7b9b374
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-10-29 19:52:54 -0300

    config: Configuração inicial da estrutura do projeto

---
+++ backend/src/test/java/com/extensao/cobra/facil/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extensao.cobra.facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
