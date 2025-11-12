aaf77674b101ecbf76b7cf7faac5dd602d9c3e75
Dan Ruan <danrsaquetti@gmail.com>
2025-11-12 20:08:13 -0300

    config: Removendo Prettier e adicionando regras do ESLint para typescript

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,12 +1,14 @@
+import eslint from '@eslint/js'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
-import prettier from 'eslint-config-prettier/flat'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  prettier,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -17,14 +19,38 @@ const eslintConfig = defineConfig([
     'node_modules/**'
   ]),
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ['**/*.{js,mjs,jsx,ts,mts,tsx}'],
     rules: {
-      quotes: ['warn', 'single'],
+      indent: ['off', 'spaces', 2],
       'linebreak-style': ['off', 'windows'],
+      quotes: ['warn', 'single'],
       semi: ['warn', 'never'],
       curly: ['warn', 'multi-line'],
+      eqeqeq: 'off',
       'no-trailing-spaces': 'warn',
+      'no-multiple-empty-lines': 'off',
+      'no-inline-comments': 'off',
+      'no-explicit-any': 'off',
+      'prefer-const': 'off',
+      'import/prefer-default-export': 'off',
+      'no-inner-declarations': 'off',
+      'no-empty-pattern': 'off',
+      'no-prototype-builtins': 'off',
+      camelcase: 'warn',
+      'no-tabs': ['error', { allowIndentationTabs: true }],
       'prettier/prettier': ['off', { endOfLine: 'auto' }],
+      'no-async-promise-executor': 'off',
+      'no-constant-condition': 'warn',
+      'no-empty': 'warn',
+      'no-unused-expressions': ['warn', { allowTaggedTemplates: true }],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-empty-interface': 'warn',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
     }
   }
 ])
