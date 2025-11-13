4152a9e4bd5ff68d7196ac713a2678177c1a4c6e
Dan Ruan <danrsaquetti@gmail.com>
2025-11-13 19:46:08 -0300

    feat: Criando layout base do Dashboard separada por Header, Sidebar e conteúdo principal

---
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Header } from '@/components/Header'
+import { Sidebar } from '@/components/Sidebar'
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      <div className="flex flex-1">
+        <Sidebar />
+
+        <main className="flex-1 p-6 bg-gray-50">{children}</main>
+      </div>
+    </div>
+  )
+}
