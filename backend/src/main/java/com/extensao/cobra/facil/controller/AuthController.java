e5d8d3928b5131d3e105f2f5befe7dad308f213d
Dan Ruan <danrsaquetti@gmail.com>
2025-11-23 21:25:12 -0300

    feat: Adicionando novo endpoint POST "/auth/register"

--- backend/src/main/java/com/extensao/cobra/facil/controller/AuthController.java
+++ backend/src/main/java/com/extensao/cobra/facil/controller/AuthController.java
@@ -3,6 +3,10 @@ package com.extensao.cobra.facil.controller;
 import com.extensao.cobra.facil.dto.login.LoginDtoRequest;
 import com.extensao.cobra.facil.dto.login.LoginDtoResponse;
 import com.extensao.cobra.facil.security.JwtService;
+import com.extensao.cobra.facil.dto.usuario.CriaUsuarioDtoRequest;
+import com.extensao.cobra.facil.entity.UsuarioEntidade;
+import com.extensao.cobra.facil.mapper.UsuarioMapper;
+import com.extensao.cobra.facil.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,13 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final UsuarioService usuarioService;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
-    public AuthController(AuthenticationManager authenticationManager,
-                          JwtService jwtService,
-                          UserDetailsService userDetailsService) {
+    public AuthController(
+            UsuarioService usuarioService,
+            AuthenticationManager authenticationManager,
+            JwtService jwtService,
+            UserDetailsService userDetailsService) {
+        this.usuarioService = usuarioService;
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
@@ -43,5 +51,12 @@ public class AuthController {
         String token = jwtService.gerarToken(user);
 
         return ResponseEntity.ok(new LoginDtoResponse(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody CriaUsuarioDtoRequest criaUsuarioDtoRequest) {
+        UsuarioEntidade usuarioEntidade = this.usuarioService
+                .criarUsuario(UsuarioMapper.usuarioEntidade(criaUsuarioDtoRequest));
+        return ResponseEntity.ok(UsuarioMapper.criaUsuarioDtoResponse(usuarioEntidade));
     }
 }
