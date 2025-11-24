053b63159896249e81fe614a239ec4a569ac0a55
Dan Ruan <danrsaquetti@gmail.com>
2025-11-23 21:04:51 -0300

    feat: Gravando o token retornado em cookies

--- frontend/src/lib/actions/auth.ts
+++ frontend/src/lib/actions/auth.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { updateTag } from 'next/cache'
+import { cookies } from 'next/headers'
 
 import { LoginUsuarioData, RegistrarUsuarioData } from '@/lib/schemas/usuario'
 import { createRequest } from '@/util/api'
@@ -10,9 +11,23 @@ import { Result } from '@/util/result'
 const request = await createRequest(env('API_URL'))
 
 export async function loginAction(data: LoginUsuarioData) {
+  const cookieStore = await cookies()
+
   const response = await request.post<{ token: string }>('/auth/login', { body: data })
 
-  return response
+  if (!response.ok) {
+    return Result.fromResult<boolean>(response)
+  }
+
+  cookieStore.set('auth_token', response.value.token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    path: '/',
+    maxAge: 60 * 60 * 24,
+  })
+
+  return Result.ok(true)
 }
 
 export async function signUpAction(data: RegistrarUsuarioData) {
