d62ecbe34fbdc8b8389f1abd2d96abb32d3f4824
Dan Ruan <danrsaquetti@gmail.com>
2025-11-25 21:07:00 -0300

    feat: Exibindo o nome do Credor/Devedor do usuário atual

--- frontend/src/components/transacao/transacoes-content.tsx
+++ frontend/src/components/transacao/transacoes-content.tsx
@@ -16,6 +16,7 @@ import {
 } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { useTransacoes } from '@/context/Transacoes'
+import { useCurrentUsuario } from '@/hooks/useCurrentUsuario'
 import { useNotification } from '@/hooks/useNotification'
 import { excluirTransacaoAction, quitarTransacaoAction } from '@/lib/actions/transaction'
 import { getDescricaoTransacaoStatus, TransacaoStatus, TransacaoWithUsuario } from '@/types/models'
@@ -26,6 +27,7 @@ export type TransacoesContentProps = {
 }
 
 export default function TransacoesContent({ transacoes, disabled }: TransacoesContentProps) {
+  const { usuario } = useCurrentUsuario()
   const { transacaoSelecionada, setTransacaoSelecionada } = useTransacoes()
   const { notify } = useNotification()
   const [isPending, startTransition] = useTransition()
@@ -64,6 +66,8 @@ export default function TransacoesContent({ transacoes, disabled }: TransacoesCo
 
   const disabledContent = isPending || disabled
 
+  console.log(transacoes)
+
   return (
     <div className='grid grid-cols-2 gap-4'>
       <CadastroTransacao transacao={transacaoSelecionada} onSuccess={() => setTransacaoSelecionada(undefined)} disabled={isPending} />
@@ -72,50 +76,66 @@ export default function TransacoesContent({ transacoes, disabled }: TransacoesCo
         <h2 className='font-semibold mb-2'>Lista</h2>
 
         <div className='space-y-2'>
-          {transacoes.map(transacao => (
-            <div key={transacao.id} className='p-2 border rounded flex justify-between items-center'>
-              <div>
-                <div className='font-medium'>R$ {transacao.valor.toFixed(2)} — {transacao.descricao}</div>
-                <div className='text-xs text-gray-500'>
-                  {transacao.dataVencimento
-                    ? `Venc.: ${new Date(transacao.dataVencimento).toLocaleDateString('pt-BR')}`
-                    : 'Sem data de venc.'} • Status: {getDescricaoTransacaoStatus(transacao.status)}
+          {transacoes.map(transacao => {
+            const isRecebeTransacao = transacao.usuarioCredor.id == usuario?.id
+
+            return (
+              <div key={transacao.id} className='p-2 border rounded flex justify-between items-center'>
+                <div className='flex items-center gap-4 w-full'>
+                  <div>
+                    <div className='font-medium'>
+                      <span className={isRecebeTransacao ? 'text-green-600 font-medium' : 'text-red-600 font-medium'}>
+                        R$ {transacao.valor.toFixed(2)}
+                      </span> — {transacao.descricao}
+                    </div>
+                    <div className='text-xs text-gray-500'>
+                      {transacao.dataVencimento
+                        ? `Venc.: ${new Date(transacao.dataVencimento).toLocaleDateString('pt-BR')}`
+                        : 'Sem data de venc.'} • Status: {getDescricaoTransacaoStatus(transacao.status)}
+                    </div>
+                  </div>
+
+                  <span>
+                    {isRecebeTransacao
+                      ? (<>De: {transacao.usuarioDevedor.nome}</>)
+                      : (<>Para: {transacao.usuarioCredor.nome}</>)}
+                  </span>
+                </div>
+
+                <div className='flex gap-1'>
+                  {transacao.status !== TransacaoStatus.QUITADA
+                    && <Button
+                      variant={'ghost'}
+                      onClick={() => quitarTransacao(transacao.id)}
+                      className='text-green-600 hover:text-green-500'
+                      size={'icon'}
+                      disabled={disabledContent}
+                      title="Quitar"
+                    >
+                      <HandCoins />
+                    </Button>
+                  }
+
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant={'ghost'} className='text-red-600 hover:text-red-500' size={'icon'} disabled={disabledContent} title='Excluir'>
+                        <Trash />
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Deseja realmente excluir a transação?</AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                        <AlertDialogAction className='bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60' onClick={() => excluirTransacao(transacao.id)}>Excluir</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
               </div>
-
-              <div className='flex gap-1'>
-                {transacao.status !== TransacaoStatus.QUITADA
-                  && <Button
-                    variant={'ghost'}
-                    onClick={() => quitarTransacao(transacao.id)}
-                    className='text-green-600 hover:text-green-500'
-                    size={'icon'}
-                    disabled={disabledContent}
-                    title="Quitar"
-                  >
-                    <HandCoins />
-                  </Button>
-                }
-
-                <AlertDialog>
-                  <AlertDialogTrigger asChild>
-                    <Button variant={'ghost'} className='text-red-600 hover:text-red-500' size={'icon'} disabled={disabledContent} title='Excluir'>
-                      <Trash />
-                    </Button>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <AlertDialogTitle>Deseja realmente excluir a transação?</AlertDialogTitle>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                      <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                      <AlertDialogAction className='bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60' onClick={() => excluirTransacao(transacao.id)}>Excluir</AlertDialogAction>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-              </div>
-            </div>
-          ))}
+            )
+          })}
 
           {transacoes.length === 0 && <div className='text-sm text-gray-500'>Nenhuma transação.</div>}
         </div>
